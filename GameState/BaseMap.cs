afdef5fdcc41b1a72e5fdb91f0268861dcd9b21e
Tyree Richardson <ker54@students.uwf.edu>
2021-04-18 14:14:13 -0500

    GameStates and BaseMap added

---
+++ GameState/BaseMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Design;
+using BaseObjects;
+
+namespace GameState
+{
+    public class BaseMap
+    {
+        public int MapId;
+        public BaseGameObject[] Tiles;
+        //public BaseGameObject[] MapObjects;
+        //public BaseGameObject[] NPC;
+
+        public virtual void LoadMap(/*This is going to be the import from content*/)
+        {
+            // this will populate each tile with their data
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var tile in Tiles)
+            {
+                
+            }
+        }
+    }
+}
---
+++ GameState/GameState.cs
@@ -0,0 +1,30 @@
+﻿using BaseObjects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameState
+{
+    class GameState : BaseState
+    {
+        Player[] players;
+        BaseMap currentMap;
+        public GameState()
+        {
+            players = new Player[1];
+            currentMap = new BaseMap();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
